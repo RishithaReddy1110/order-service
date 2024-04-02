b53af68166c21a8739342c7910f1039afa12d31a
RishithaReddy1110 <85563486+RishithaReddy1110@users.noreply.github.com>
2024-04-02 13:38:19 +0530

    Delete order-service directory

--- order-service/src/main/java/com/order/in/common/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.order.in.common;
-
-public enum PaymentStatus {
-    SUCCESS, FAILED
-}
--- order-service/src/main/java/com/order/in/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.order.in.repository;
-
-import com.order.in.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
